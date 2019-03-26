012ee57770584f0ba9a0f53488ea239f03928a17
AndyFernandes <andfernandes24@gmail.com>
2019-03-26 01:57:13 -0300

    [+] Continuação estudo BFRTP

--- QuickSort/BFPRT.cpp
+++ QuickSort/BFPRT.cpp
@@ -4,12 +4,6 @@
 #include<tuple> // for tuple 
 using namespace std;
 
-void trocar(int* posicao1, int *posicao2){
-  int aux = *posicao1;
-  *posicao1 = *posicao2;
-  *posicao2 = aux;
-}
-
 void print_vector(int *inicio, int *fim){
     cout << "-------------------------------------\n\t\t";
     for(; inicio != fim; inicio++){
@@ -18,21 +12,71 @@ void print_vector(int *inicio, int *fim){
     cout << "\n-------------------------------------\n";
 }
 
+void trocar(int* posicao1, int *posicao2){
+  int aux = *posicao1;
+  *posicao1 = *posicao2;
+  *posicao2 = aux;
+}
+
+int* Particao_Lomuto(int *inicio, int *pivo, int *fim){
+    int *m = inicio, *j = inicio + 1, aux = *inicio;
+    *inicio = *pivo;
+    *pivo = aux;
+
+    for(;j != fim+1; ++j){
+       if (*j < *inicio){
+            m++;
+            aux = *j;
+            *j = *m;
+            *m = aux; 
+       }
+    }
+    
+    aux = *inicio;
+    *inicio = *m;
+    *m = aux;
+
+    return m;
+}
+
+void Selecao_Hoare(int *inicio, int *fim, int *i){
+    if(inicio < fim) {
+        int *pivo = Particao_Lomuto(inicio, fim, fim);
+        if(i < pivo)
+            Selecao_Hoare(inicio, pivo-1, i); // chamada a esquerda
+        if(i > pivo)
+            Selecao_Hoare(pivo+1, fim, i); // chamada a direita
+    }
+}
+
+
+
 /*
       1.  Colete os elementos da matriz em grupos de 5. Se o número de elementos na matriz não for um múltiplo de 5, crie um grupo adicional (que conterá no máximo 4 elementos).
       2.  Encontre a mediana de cada grupo.
       3.  Por meio de uma chamada recursiva, encontre a mediana das medianas.
       4.  Use a mediana das M medianas como um pivô e chame o algoritmo recursivamente na matriz apropriada, exatamente como no algoritmo quickselect .
 */
-void BFPRT(int* inicio, int* fim){
-  int len = fim - inicio;
+// DOING
+void BFPRT(int* inicio, int* fim, int *pivo){
+  int tamanho_vetor_ponteiros = abs((fim - inicio)/5) + 1;
   // vou montar um vetor pra pegar os ponteiros pros elementos ou então definir uma struct
-  cout << abs(len/5);
+  int* medianas[tamanho_vetor_ponteiros];
+  for(int i = 0; i < tamanho_vetor_ponteiros; ++i){
+    medianas[i] = inicio + i*5;
+  }
 }
 
 // DOING 
-void QuickSort_BFPRT(int *inicio, int *i, int *fim){
-
+void QuickSort_BFPRT(int *inicio, int *pivo, int *fim){
+   if(inicio < fim) {
+        BFPRT(inicio, pivo, fim);
+        if(pivo > inicio)
+            QuickSort_BFPRT(inicio, pivo-1, pivo-1); // chamada a esquerda
+        
+        if(pivo < fim)
+            QuickSort_BFPRT(pivo+1, fim, fim); // chamada a direita
+    }
 }
 
 int main(){
