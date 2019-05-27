bdbaa99a85b89540583ba3ce4ac77ca7f660a7e2
andyfernandes <andfernandes24@gmail.com>
2019-05-27 17:25:27 -0300

    [+] Contando caracteres

--- CodificacaoHuffman/Trabalho.cpp
+++ CodificacaoHuffman/Trabalho.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <map>
 #include "Heap.cpp"
+#include <set>
+#include <algorithm>
+#include <functional>
 using namespace std;
+typedef map<char, int> dict;
+typedef pair<char, int> par;
+// typedef function<bool(dict, dict)> comparator;
 
 void print_vector(int *inicio, int *fim){
     cout << "\n";
@@ -10,22 +17,44 @@ void print_vector(int *inicio, int *fim){
     cout << "\n";
 }
 
+dict countChar(char* pText){
+    dict m;
+    while(*pText != '\0'){
+        if (m[*pText])
+            m[*pText] += 1;
+        else
+            m[*pText] = 1;
+        pText++;
+    }
+    return m;
+}
+
 int main(){
     // int vetor[] =  {28,58,49,69,52,69,40,75,71,82};
-    int vetor[] =  {4,1,3,2,16,9,10,14,8,7};
+    // int vetor[] =  {4,1,3,2,16,9,10,14,8,7};
     Heap heap;
-    heap.construir(vetor, 10);
-    // print_vector(vetor, vetor+15);
-    print_vector(heap.heap, heap.heap + heap.heapSize);
-    cout << "Minimo " << heap.minimum() << endl;
-    int min = heap.extractMinimum();
-    cout << "Minimo " << min << endl;
-    print_vector(heap.heap, heap.heap + heap.heapSize);
+    // heap.construir(vetor, 10);
+    // // print_vector(vetor, vetor+15);
+    // print_vector(heap.heap, heap.heap + heap.heapSize);
+    // cout << "Minimo " << heap.minimum() << endl;
+    // int min = heap.extractMinimum();
+    // cout << "Minimo " << min << endl;
+    // print_vector(heap.heap, heap.heap + heap.heapSize);
 
-    heap.insert(3);
-    heap.insert(50);
-    heap.insert(7);
-    heap.insert(6);
+    // heap.insert(3);
+    // heap.insert(50);
+    // heap.insert(7);
+    // heap.insert(6);    
+    // dict countSymbols;
+    // m['A'] = 2;
 
+    dict countSymbols = countChar("A arina e louca");
+    
+    for(par element: countSymbols){
+    //    cout << element.first << " : " << element.second << endl;
+        heap.insert(element.second);
+    }
+
+    // TODO FAZER UMA STRUCT
     return 0;
-}    
+}
