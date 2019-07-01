eabf97e2292ba00a95c369c32c35ab649d95eafa
andyfernandes <andfernandes24@gmail.com>
2019-07-01 17:21:39 -0300

    [+] Inserção + casos de teste que o Pablo mandou pra Arina

--- DictWithAVL/AVL.cpp
+++ DictWithAVL/AVL.cpp
@@ -1,6 +1,7 @@
+#include <iostream>
 #include <stdio.h>
 #include "AVL.hpp"
-
+using namespace std;
 // Inicializa D como uma árvore vazia.
 void inicializar (DicAVL &D){
     D.raiz = nullptr;
@@ -15,7 +16,28 @@ int max(int a, int b){
     return (a > b)? a : b;
 }
 
-Noh* rotationLeft(Noh *x){
+bool isLeaf(Noh* no){
+    if(no->esq == nullptr && no->dir == nullptr) return true;
+    return false;
+}
+
+Noh* newNo(TC chave, TV value, Noh* pai, Noh* esq, Noh* dir, int h){
+    Noh *no;
+    no->chave = chave;
+    no->valor = value;
+    no->pai = pai;
+    no->esq = esq;
+    no->dir = dir;
+    no->h = h;
+    return no;
+}
+
+int getBalance(Noh* no){
+    if(no == nullptr) return 0;
+    return height(no->esq) - height(no->dir);
+}
+
+Noh* rotationLeft(DicAVL dicionario, Noh *x){
     Noh* paiX = x->pai;
     Noh* y = x->dir;
     Noh* filhoEsqY = y->esq;
@@ -24,7 +46,7 @@ Noh* rotationLeft(Noh *x){
     if(paiX != nullptr){
         if(paiX->esq == x) paiX->esq = y;
         else if(paiX->dir == x) paiX->dir = y;
-    }
+    } else dicionario.raiz = y;
 
      // X -> muda pai e muda filho direito
     x->pai = y;
@@ -44,7 +66,7 @@ Noh* rotationLeft(Noh *x){
     return y; 
 }
 
-Noh* rotationRight(Noh *y){
+Noh* rotationRight(DicAVL dicionario, Noh *y){
     Noh* paiY = y->pai;
     Noh* x = y->esq;
     Noh* filhoDirX = x->dir;
@@ -53,7 +75,7 @@ Noh* rotationRight(Noh *y){
     if(paiY != nullptr){ // Atualizando ponteiro do filho do pai de Y para no lugar de y ser x
         if(paiY->esq == y) paiY->esq = x;
         else if(paiY->dir == y) paiY->dir = x;
-    }
+    } else dicionario.raiz = x;
 
     // X -> muda pai e muda filho direito
     x->pai = paiY;
@@ -86,23 +108,66 @@ Noh* procurar(DicAVL &D, TC c){
     return nullptr;
 }
 
-bool isLeaf(Noh* no){
-    if(no->esq == nullptr && no->dir == nullptr) return true;
-    return false;
+void atualizarAlturas(Noh* no){
+    if(no != nullptr) return;
+    no->h = max(height(no->esq), height(no->dir)) + 1;
+    atualizarAlturas(no->pai);
 }
 
 // Retorna um ponteiro para o novo nó ou nulo se erro de alocação
-Noh* inserir (DicAVL &D, TC c, TV v){
+Noh* inserir(DicAVL &D, TC c, TV v){
+    Noh* novoNo = newNo(c, v, nullptr, nullptr, nullptr, 1);
+    Noh* root = D.raiz;
+    Noh* noAnterior;
 
+    // caso a. Raiz nula => raiz é o nó
+    if(root == nullptr) D.raiz = novoNo;
+
+    // caso b. Raiz não nula => Insere e realiza balanceamento
+    
+    // Pega o nó a quem vou adicionar o novo nó
+    while(root != nullptr){
+        noAnterior = root;
+        if(c < root->chave) root = root->esq;
+        else if(c > root->chave) root = root->dir;
+    }
+
+    root = noAnterior;
+
+    // Insere novo nó
+    if(c < root->chave) root->esq = novoNo;
+    else if(c > root->chave) root->dir = novoNo;
+
+    // Ajuste de ponteiros de pai
+    novoNo->pai = root;
+
+    // Propagando o +1 nas alturas dos nós posteriores ao inserido e balanceando a árvore (caso esteja desbalanceada)
+    do {
+        root->h = max(height(root->esq), height(root->dir)) + 1;
+        int balance = getBalance(root);
+
+        if(balance > 1 && c < root->esq->chave) root = rotationRight(D, root); // Left Left Case
+        else if(balance < -1 && c > root->dir->chave) root = rotationLeft(D, root); // Right Right Case
+        else if(balance > 1 && c > root->esq->chave){ // Left Right Case
+            root->esq = rotationLeft(D, root->esq);
+            root = rotationRight(D, root);
+        } else if(balance < -1 && c < root->dir->chave){ // Right Left Case
+            root->dir = rotationRight(D, root->dir);
+            root = rotationLeft(D, root);
+        }
+        root = root->pai;
+        cout << root->chave;
+    } while(root!=nullptr);
+    return novoNo;
 } 
 
 // n aponta para o nó a ser removido
-void remover (DicAVL &D, Noh *n){
+void remover(DicAVL &D, Noh *n){
 
 } 
 
 // Desaloca os nós da árvore.
-void terminar (DicAVL &D){
+/*void terminar (DicAVL &D){
     // acessar a raiz do dicionario e visitar em pré-ordem desalocando os nós
     Noh* no = D.raiz;
 
@@ -111,4 +176,4 @@ void terminar (DicAVL &D){
         else if(chave > c) no = no->esq;
         else if(chave < c) no = no->dir;
     }
-} 
+} */
--- DictWithAVL/main.cpp
+++ DictWithAVL/main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
-#include "AVL.hpp"
+#include "AVL.cpp"
 using namespace std;
 
 int main(){
+    DicAVL dicionario;
+    inicializar(dicionario);
+    Noh *no1 = inserir(dicionario, 204, -21);
+    // Noh *no2 = inserir(dicionario, 169.75, 59.5);
+    // Noh *no3 = inserir(dicionario, 246.75, -60);
+    // Noh *no4 = inserir(dicionario, -98.75, 53.5);
+    // Noh *no5 = inserir(dicionario, -133.25, 10.5);
+    // Noh *no6 = inserir(dicionario, -13.5, 25.5);
+    // Noh *no7 = inserir(dicionario, -81, 2.5);
+    // Noh *no8 = inserir(dicionario, 271.25, 83);
+    // remover(dicionario, no3);
+    // procurar(dicionario, 246.75);
+    // remover(dicionario, no8);
+    // procurar(dicionario, 271.25);
+    // remover(dicionario, no7);
+    // procurar(dicionario, -81);
+    // Noh *no9 = inserir(dicionario, 182.5, -68.5);
+    // Noh *no10 = inserir(dicionario, -59.25, -74);
+    // Noh *no11 = inserir(dicionario, -234.5, 99.5);
+    // remover(dicionario, no11);
+    // procurar(dicionario, -234.5);
+    // remover(dicionario, no9);
+    // procurar(dicionario, 182.5);
+    // remover(dicionario, no1);
+    // procurar(dicionario, 204);
+    // remover(dicionario, no6);
+    // procurar(dicionario, -13.5);
+    // remover(dicionario, no5);
+    // procurar(dicionario, -133.25);
+    // remover(dicionario, no4);
+    // procurar(dicionario, -98.75);
+    // remover(dicionario, no11);
+    // procurar(dicionario, -169.75);
+    // remover(dicionario, no2);
+    // procurar(dicionario, -59.25);
 
+    cout << dicionario.raiz->chave;
     return 0;
 }
