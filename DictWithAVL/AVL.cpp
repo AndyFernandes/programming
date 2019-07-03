372c406e2e9a4ef26f8d829569731ad0a93be220
andyfernandes <andrezafernandes@alu.ufc.br>
2019-07-03 16:27:39 -0300

    [+] Ajeitando os casos bases da remoção e iniciando o caso geral

--- DictWithAVL/AVL.cpp
+++ DictWithAVL/AVL.cpp
@@ -119,7 +119,7 @@ Noh* procurar(DicAVL &D, TC c){
         else if(chave < c) no = no->dir;
     }
 
-    cout << "No nao encontrado!" << endl;
+    cout << "Chave "<< c <<" nao encontrado!" << endl;
     return nullptr;
 }
 
@@ -142,9 +142,7 @@ Noh* inserir(DicAVL &D, TC c, TV v){
         D.raiz = novoNo;
         return novoNo;
     } 
-    
     // caso b. Raiz não nula => Insere e realiza balanceamento
-    
     // Pega o nó a quem vou adicionar o novo nó
     while(root != nullptr){
         noAnterior = root;
@@ -159,14 +157,12 @@ Noh* inserir(DicAVL &D, TC c, TV v){
     }
 
     root = noAnterior;
-
     // Ajuste de ponteiros de pai
     novoNo->pai = root;
 
     // Insere novo nó
     if(c < root->chave) root->esq = novoNo;
     else if(c > root->chave) root->dir = novoNo;
-
     // Propagando o +1 nas alturas dos nós posteriores ao inserido e balanceando a árvore (caso esteja desbalanceada)
     do {
         // cout << "Passei aqui3" << endl;
@@ -177,21 +173,21 @@ Noh* inserir(DicAVL &D, TC c, TV v){
 
         if(balance > 1 && c < root->esq->chave){
         // if(balance > 1 && ladoAdicionado == true){
-            cout << "IF 1" << endl;
+            // cout << "IF 1" << endl;
             root = rotationRight(D, root); // Left Left Case
         } else if(balance < -1 && c > root->dir->chave){
         // else if(balance < -1 && ladoAdicionado == false){
-            cout << "IF 2" << endl;
+            // cout << "IF 2" << endl;
             // cout << root-> chave << endl;
             root = rotationLeft(D, root); // Right Right Case
         } else if(balance > 1 && c > root->esq->chave){ // Left Right Case
         // else if(balance > 1 && ladoAdicionado == false){ // Left Right Case
-            cout << "IF 3" << endl;
+            // cout << "IF 3" << endl;
             root->esq = rotationLeft(D, root->esq);
             root = rotationRight(D, root);
         }else if(balance < -1 && c < root->dir->chave){ // Right Left Case 
         // else if(balance < -1 && ladoAdicionado == true){ // Right Left Case
-            cout << "IF 4" << endl;
+            // cout << "IF 4" << endl;
             // cout << root->chave << endl;
             root->dir = rotationRight(D, root->dir);
             root = rotationLeft(D, root);
@@ -206,73 +202,73 @@ Noh* inserir(DicAVL &D, TC c, TV v){
 // Retorna o nó maissss a esquerda do nó passado
 Noh* minNode(Noh* no){
     Noh* corrente = no;
-    while(corrente->esq != nullptr)
-        corrente = corrente->esq;
+    while(corrente->esq != nullptr) corrente = corrente->esq;
     return corrente;
 }
 
 // n aponta para o nó a ser removido
-/*void remover(DicAVL &D, Noh *n){
+void remover(DicAVL &D, Noh *n){
+    if(n == nullptr) return;
+
     Noh* root = D.raiz;
-    Noh* noAnterior = nullptr;
     int balance;
     // se a raiz for nula.. nada a remover
     if(root == nullptr) return;
 
     // Verificando se o nó realmente pertence ao dicionário
-    Noh* n = procurar(D, n->chave);
+    n = procurar(D, n->chave);
     if(n == nullptr) return;
+    Noh* paiN = n->pai;
 
-    // Caso base: é a raiz da árvore ou é um nó que tem apenas uma subarv esquerda ou direita
-    if(n->pai == nullptr && n->esq != nullptr && n->dir == nullptr){ // nó raiz com 1 filho
-        D.raiz = n->esq;
-        n->esq->pai = nullptr;
-        free(n);
-        return;
-    } else if(n->pai == nullptr && n->dir != nullptr && n->esq == nullptr){ // nó raiz com 1 filho
-        D.raiz = n->dir;
-        n->dir->pai = nullptr;
-        free(n);
-        return;
-    } else if(n->pai == nullptr && n->dir == nullptr && n->esq == nullptr){ // nó raiz sem filhos
-        D.raiz = nullptr;
-        free(n);
-        return;
-    } else { // nó com 2 filhos
-        Noh* temp = minNode(n->dir);
+    // Caso base a: é a raiz da árvore ou é um nó que tem apenas uma subarv DE 1 FOLHA esquerda ou direita 
+    // a.1. É A RAIZ 
+    if(n->pai == nullptr){
+        if(n->esq != nullptr && n->dir == nullptr && n->esq->esq == nullptr && n->esq->dir == nullptr){ // nó raiz com 1 filho
+            D.raiz = n->esq;
+            n->esq->pai = nullptr;
+            delete(n);
+            return;
+        } else if(n->dir != nullptr && n->esq == nullptr && n->dir->esq == nullptr && n->dir->dir == nullptr){ // nó raiz com 1 filho
+            D.raiz = n->dir;
+            n->dir->pai = nullptr;
+            delete(n);
+            return;
+        } else if(n->dir == nullptr && n->esq == nullptr){ // nó raiz sem filhos
+            D.raiz = nullptr;
+            delete(n);
+            return;
+        }
+    } else { // a.2. NÃO É A RAIZ
+        if(n->esq != nullptr && n->dir == nullptr && n->esq->esq == nullptr && n->esq->dir == nullptr){ // Mas possui apenas 1 filho esquerdo, que é folha
+            n->esq->pai = paiN;
+            // TODO: vê se é necessário ajustar altura pro pai de n
+            delete(n);
+            return;
+        } else if(n->dir != nullptr && n->esq == nullptr && n->dir->esq == nullptr && n->dir->dir == nullptr){ // Mas possui apenas 1 filho direito, que é folha
+            n->dir->pai = paiN;
+            // TODO: vê se é necessário ajustar altura pro pai de n
+            delete(n);
+            return;
+        } else if(n->dir == nullptr && n->esq == nullptr){ // nó sem filhos -> CASO DO NÓ FOLHA: CASO 2
+            // TODO: vê se é necessário ajustar altura pro pai de n
+            delete(n);
+            return;
+         } else { // nó com 2 filhos
+            Noh* temp = minNode(n->dir); // Pega o próximo nó que é maior que o nó n
+            do {
+                balance = getBalance(n);
+                if(balance == 1 || balance == 0){
+                    if(n->dir->h > n->esq->h){ // pega o nó da árvore direita pra substituir x
+                        
+                    } else { // pega o nó da árvore esquerda pra substituir x
 
-        do {
-            balance = getBalance(n);
-            if(balance == 1 || balance == 0){
-                if(n->dir->h > n->esq->h){ // pega o nó da árvore direita pra substituir x
-                    
-                } else { // pega o nó da árvore esquerda pra substituir x
-
+                    }
                 }
-            }
 
-        } while(n != nullptr);
+            } while(n != nullptr);
+        }
     }
-
-    // Case base2: é um nó folha
-    if(isLeaf(n)){
-        free(n);
-        if(n->pai->esq == n) n->pai->esq = nullptr;
-        else if(n->pai->dir == n) n->pai->dir = nullptr;
-        // TODO: atualizar altura do resto da árvore
-        // TODO2: Ver se é necessário balancear
-    }
-
-
-
-
-    do{
-
-    }while();
-
-
-
-} */
+}
 
 // Desaloca os nós da árvore.
 /*void terminar (DicAVL &D){
--- DictWithAVL/main.cpp
+++ DictWithAVL/main.cpp
@@ -17,6 +17,7 @@ void preOrder(Noh *root){
 int main(){
     DicAVL dicionario;
     inicializar(dicionario);
+    ///////////////////////////////  TESTES INSERÇÃO /////////////////////////////
     Noh* no = inserir(dicionario, 10, 10);
     no = inserir(dicionario, 20, 10);
     no = inserir(dicionario, 5, 5);
@@ -24,56 +25,64 @@ int main(){
     no = inserir(dicionario, 40, 10);
     no = inserir(dicionario, 50, 10);
     no = inserir(dicionario, 2, 10);
-    // cout << endl << endl;
-    
-    // cout << endl << endl;
+    Noh *no1 = inserir(dicionario, 204, -21);
+    Noh *no2 = inserir(dicionario, 169.75, 59.5);
+    Noh *no3 = inserir(dicionario, 246.75, -60);
+    Noh *no4 = inserir(dicionario, -98.75, 53.5);
+    Noh *no5 = inserir(dicionario, -133.25, 10.5);
+    Noh *no6 = inserir(dicionario, -13.5, 25.5);
+    Noh *no7 = inserir(dicionario, -81, 2.5);
+    Noh *no8 = inserir(dicionario, 271.25, 83);
+    Noh *no9 = inserir(dicionario, 182.5, -68.5);
+    Noh *no10 = inserir(dicionario, -59.25, -74);
+    Noh *no11 = inserir(dicionario, -234.5, 99.5);
     no = inserir(dicionario, 25, 10);
-    preOrder(dicionario.raiz);
+    preOrder(dicionario.raiz);  
 
+    ///////////////////////////////  TESTES PROCURAR /////////////////////////////
     cout << endl << endl << endl;
     no = procurar(dicionario, 10);
-    cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
-    // Noh *no1 = inserir(dicionario, 204, -21);
-    // // cout << dicionario.raiz->chave << endl;
-    // Noh* no = procurar(dicionario, 230);
-    // // cout << no->chave;
-    // Noh *no2 = inserir(dicionario, 169.75, 59.5);
-    // // cout << dicionario.raiz->chave << endl;
-    // no = procurar(dicionario, 169.75);
-    // cout << no->chave << endl;
-    // Noh *no3 = inserir(dicionario, 246.75, -60);
-    // Noh *no4 = inserir(dicionario, -98.75, 53.5);
+    if(no != nullptr) cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
+    no = procurar(dicionario, 230);
+    if(no != nullptr) cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
+    no = procurar(dicionario, 169.75);
+    if(no != nullptr) cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
+    no = procurar(dicionario, 246.75);
+    if(no != nullptr) cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
+    no = procurar(dicionario, 271.25);
+    if(no != nullptr) cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
+    no = procurar(dicionario, -81);
+    if(no != nullptr) cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
+    no = procurar(dicionario, -234.5);
+    if(no != nullptr) cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
+    no = procurar(dicionario, 182.5);
+    if(no != nullptr) cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
+    no = procurar(dicionario, 204);
+    if(no != nullptr) cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
+    no = procurar(dicionario, -13.5);
+    if(no != nullptr) cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
+    no = procurar(dicionario, -133.25);
+    if(no != nullptr) cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
+    no = procurar(dicionario, -98.75);
+    if(no != nullptr) cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
+    no = procurar(dicionario, -169.75);
+    if(no != nullptr) cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
+    no = procurar(dicionario, -59.25);
+    if(no != nullptr) cout << "CHAVE: "  << no->chave << " | " << no->valor << endl;
     
-    // Noh *no5 = inserir(dicionario, -133.25, 10.5);
-    // preOrder(dicionario.raiz);
-    // Noh *no6 = inserir(dicionario, -13.5, 25.5);
+    ///////////////////////////////  TESTES REMOÇÃO /////////////////////////////
     
-    // Noh *no7 = inserir(dicionario, -81, 2.5);
-    // Noh *no8 = inserir(dicionario, 271.25, 83);
     // remover(dicionario, no3);
-    // procurar(dicionario, 246.75);
     // remover(dicionario, no8);
-    // procurar(dicionario, 271.25);
     // remover(dicionario, no7);
-    // procurar(dicionario, -81);
-    // Noh *no9 = inserir(dicionario, 182.5, -68.5);
-    // Noh *no10 = inserir(dicionario, -59.25, -74);
-    // Noh *no11 = inserir(dicionario, -234.5, 99.5);
     // remover(dicionario, no11);
-    // procurar(dicionario, -234.5);
     // remover(dicionario, no9);
-    // procurar(dicionario, 182.5);
     // remover(dicionario, no1);
-    // procurar(dicionario, 204);
     // remover(dicionario, no6);
-    // procurar(dicionario, -13.5);
     // remover(dicionario, no5);
-    // procurar(dicionario, -133.25);
     // remover(dicionario, no4);
-    // procurar(dicionario, -98.75);
     // remover(dicionario, no11);
-    // procurar(dicionario, -169.75);
     // remover(dicionario, no2);
-    // procurar(dicionario, -59.25);
+    
     return 0;
 }
